79f24849176d02ee1b20ba0fff608dbf606fbbaa
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-08-22 14:09:30 +0000

    digitize util function

--- src/utils/utilityFunctions.js
+++ src/utils/utilityFunctions.js
@@ -1,8 +1,18 @@
-export const digitizeNumber = (input) => {
-    return input < 10 ? `0` + input : input;
-};
+export function digitizeNumber(input, digits = 2) {
+	const length_difference = digits - String(input).length;
+  if(length_difference < 1) {
+  	throw `digitize(${input}, ${digits}) error\ninput longer than target digits`
+  } else {
+  	let output = String(input);
+  	for(i = 0; i < length_difference; i++) {
+    	output = "0" + output;
+    }
+    return output;
+  }
+}
 
 export const getCurrentMMSS = () => {
     const currentTime = new Date();
     return `${digitizeNumber(currentTime.getMinutes())}:${digitizeNumber(currentTime.getSeconds)}`;
 }
+
