0e2301e85f2b00ff58151180e5b2bdf05094b1dc
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-08-23 17:24:09 +0000

    implemented call pending timer

--- src/components/CallRow.jsx
+++ src/components/CallRow.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import { getMMSS } from "../utils/utilityFunctions";
+
 export default function CallRow(props) {
+
+    const [pending, setPending] = useState("--:--");
+
+    useEffect(() => {
+        const intervalid = setInterval(() => {
+            setPending(getMMSS(Date.now() - props.callDetails.date_created));
+        }, 1000);
+        return () => clearInterval(intervalid);
+    }, [])
+
     return <tr key={props.incident_number}>
         <td>{props.callDetails.priority}</td>
-        <td></td> {/* function calculating pending time since call added */}
+        <td>{pending}</td>
         <td>{props.callDetails.callType}</td>
         <td>{props.callDetails.address}</td>
         <td>{props.callDetails.incident_number}</td>
