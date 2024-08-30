b46a6eaae815aa37d2fd21506cc699808f6e3a44
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-08-30 19:43:05 +0000

    unitrow td, classname with unit status

--- src/components/UnitRow.jsx
+++ src/components/UnitRow.jsx
@@ -1,11 +1,12 @@
 export default function UnitRow(props) {
-    return <tr className="hcad_unitrow">
+    return <tr className={`hcad_unitrow hcad_unitrow_status${props.unitDetails.status}`}>
         <td className="hcad_unitrow_td_unit">{props.unitDetails.unit}</td>
         <td className="hcad_unitrow_td_idle">{/*props.unitDetails.idle*/}</td>
         <td className="hcad_unitrow_td_status">{props.unitDetails.status}</td>
         <td className="hcad_unitrow_td_officer">{props.unitDetails.officer2 === "" ? props.unitDetails.officer1 : `${props.unitDetails.officer1}, ${props.unitDetails.officer2}`}</td>
         <td className="hcad_unitrow_td_badge">{props.unitDetails.badge2 === "" ? props.unitDetails.badge1 : `${props.unitDetails.badge1}, ${props.unitDetails.badge2}`}</td>
         <td className="hcad_unitrow_td_location">{props.unitDetails.location}</td>
-        {/* <td className="hcad_unitrow_td_unit">{props.unitDetails.unit}</td> */}
+        <td className="hcad_unitrow_td_calltype">{props.unitDetails.incidentType}</td>
+        <td className="hcad_unitrow_td_incnum">{props.unitDetails.incidentNumber}</td>
     </tr>
 }
