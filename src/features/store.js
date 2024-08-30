26b696e6affb3338eee89ebf0111999146749e52
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-08-30 19:22:35 +0000

    unitlist table initialized

--- src/features/store.js
+++ src/features/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import callFormSlice from "./callFormSlice";
+import unitSlice from "./unitSlice";
 
 export default configureStore({
     reducer: {
-        "callList": callFormSlice
+        "callList": callFormSlice,
+        "units": unitSlice
     }
 });
