779568a083bbefc9cc65d05f626652fa0d9999a5
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-08-30 19:38:09 +0000

    added unitlocation

--- src/components/UnitRow.jsx
+++ src/components/UnitRow.jsx
@@ -5,9 +5,7 @@ export default function UnitRow(props) {
         <td className="hcad_unitrow_td_status">{props.unitDetails.status}</td>
         <td className="hcad_unitrow_td_officer">{props.unitDetails.officer2 === "" ? props.unitDetails.officer1 : `${props.unitDetails.officer1}, ${props.unitDetails.officer2}`}</td>
         <td className="hcad_unitrow_td_badge">{props.unitDetails.badge2 === "" ? props.unitDetails.badge1 : `${props.unitDetails.badge1}, ${props.unitDetails.badge2}`}</td>
-        {/* <td className="hcad_unitrow_td_unit">{props.unitDetails.unit}</td>
-        <td className="hcad_unitrow_td_unit">{props.unitDetails.unit}</td>
-        <td className="hcad_unitrow_td_unit">{props.unitDetails.unit}</td>
-        <td className="hcad_unitrow_td_unit">{props.unitDetails.unit}</td> */}
+        <td className="hcad_unitrow_td_location">{props.unitDetails.location}</td>
+        {/* <td className="hcad_unitrow_td_unit">{props.unitDetails.unit}</td> */}
     </tr>
 }
