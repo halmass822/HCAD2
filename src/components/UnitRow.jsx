18f5a3107415c2900bbe174fe2a32193e14f0707
ForeverNumbered <xxchromechops@gmail.com>
2024-09-03 14:41:23 -0400

    unit idle timer

--- src/components/UnitRow.jsx
+++ src/components/UnitRow.jsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import { getMMSS } from "../utils/utilityFunctions";
+
 export default function UnitRow(props) {
+
+    const [pending, setPending] = useState("--:--");
+
+    useEffect(() => {
+        const intervalid = setInterval(() => {
+            setPending(getMMSS(Date.now() - props.unitDetails.idle));
+        }, 1000);
+        return () => clearInterval(intervalid);
+    }, []);
+
     return <tr className={`hcad_unitrow hcad_unitrow_status${props.unitDetails.status}`}>
         <td className="hcad_unitrow_td_unit">{props.unitDetails.unit}</td>
-        <td className="hcad_unitrow_td_idle">{/*props.unitDetails.idle*/}</td>
+        <td className="hcad_unitrow_td_idle">{[pending]}</td>
         <td className="hcad_unitrow_td_status">{props.unitDetails.status}</td>
         <td className="hcad_unitrow_td_officer">{props.unitDetails.officer2 === "" ? props.unitDetails.officer1 : `${props.unitDetails.officer1}, ${props.unitDetails.officer2}`}</td>
         <td className="hcad_unitrow_td_badge">{props.unitDetails.badge2 === "" ? props.unitDetails.badge1 : `${props.unitDetails.badge1}, ${props.unitDetails.badge2}`}</td>
