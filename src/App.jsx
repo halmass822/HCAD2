d8080113a063037eafdf1da800ad0e40791a35a0
Hazem Al Massri <93576140+halmass822@users.noreply.github.com>
2024-09-16 13:55:59 +0000

    edit unit overlay

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import CallForm from './components/CallForm';
 import CallList from './components/CallList';
+import EditUnitOverlay from './components/EditUnitOverlay';
 import { UnitList } from './components/UnitList';
 
 
 function App() {
   return <div id="hcad_app">
+    <EditUnitOverlay />
     <div id="hcad_topdiv">
       <CallForm />
       <CallList />
---
+++ src/components/EditUnitOverlay.jsx
@@ -0,0 +1,9 @@
+import "./EditUnitOverlay.css";
+
+export default function EditUnitOverlay() {
+    return <div id="hcad_overlay">
+        <div id="hcad_editunitoverlay">
+            <labe>hmmm<input type="text"></input></labe>
+        </div>
+    </div>
+}
